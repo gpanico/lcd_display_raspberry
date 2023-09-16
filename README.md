f0b27706633763790db364f6e4d583968ec1acf6
Gianfranco Panico <panico@linuz.it>
2023-09-16 18:38:08 +0200

    moved to py3

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Do you have a short wave receiver and a 1$ USB sound dongle?
 
 Tune your short wave receiver on 10.100 MHz USB, and receive the weather forecast on the display! 
 
+## requirements
+
+1. raspberry PI, raspbian, I2c modules on
+2. I2c display
+3. python + smbus
+4. minimodem (sudo apt install minimodem)
+
--- read.py
+++ read.py
@@ -1,30 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 import time
 import os
 import array as arr
 
-rowA=arr.array('c', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
-rowB=arr.array('c', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
+rowA=arr.array('u', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
+rowB=arr.array('u', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
 
 def print_display(row1, row2):
     os.system("clear")
     for i in range(0,16):
-        print(row1[i]),
-    print(" ")
+        print(row1[i],end='')
+    print("")
     for i in range(0,16):
-        print(row2[i]),
-    print(" ")
-    print(" ")
+        print(row2[i],end='')
+    print("")
 
 ##########################################
 
 x=0
-
 while True:
     
     c=sys.stdin.read(1)
-    time.sleep(0.3)
+    time.sleep(0.2)
 
     if c.isalpha(): 
         rowB[x]=c
@@ -42,5 +40,4 @@ while True:
             rowB[i]="_"
 
     print_display(rowA, rowB) 
-
 
