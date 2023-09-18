4d372cbc05f0e9f0a26b7cbe007f000adc72e460
Gianfranco Panico <panico@linuz.it>
2023-09-18 09:46:13 +0200

    getting ready to drive the display

--- read.py
+++ read.py
@@ -3,9 +3,11 @@ import sys
 import time
 import os
 import array as arr
+import I2C_LCD_driver
 
 disp_col=20
 disp_row=4
+#mylcd=I2C_LCD_driver.lcd()
 
 rowA=arr.array('u', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
 rowB=arr.array('u', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"]) 
@@ -15,25 +17,30 @@ rowD=arr.array('u', ["_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"
 def print_display(row1, row2, row3, row4):
     os.system("clear")
     for i in range(disp_col):
-        print(row1[i],end='')
+        print(row1[i],'',end='')
+        #mylcd.lcd_display_string(row1[i], 4, i) 
     print("")
     for i in range(disp_col):
-        print(row2[i],end='')
+        print(row2[i],'',end='')
+        #mylcd.lcd_display_string(row2[i], 4, i) 
     print("")
     for i in range(disp_col):
-        print(row3[i],end='')
+        print(row3[i],'',end='')
+        #mylcd.lcd_display_string(row3[i], 4, i) 
     print("")
     for i in range(disp_col):
-        print(row4[i],end='')
+        print(row4[i],'',end='')
+        #mylcd.lcd_display_string(row4[i], 4, i) 
     print("")
 
 ##########################################
 
+print_display(rowA, rowB, rowC, rowD) 
 x=0
 while True:
     
     c=sys.stdin.read(1)
-    time.sleep(0.2)
+    time.sleep(0.15)
 
     if c.isalpha(): 
         rowD[x]=c
@@ -42,8 +49,10 @@ while True:
     else:
         rowD[x]="~"
 
-    x=x+1
+    # print new char on the last line of display
+    # lcd_display_string(self, rowD[x], 4, x) 
 
+    x=x+1
     if x>(disp_col-1): 
         x=0
         for i in range(disp_col):
