cedc0bb5a4578ea7aa0ade16fc892a673b833a9c
Gianfranco Panico <panico@linuz.it>
2023-10-05 17:09:59 +0100

    fixed print_display

--- read.py
+++ read.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import sys
 from time import sleep
 from os import system
@@ -9,17 +10,16 @@ disp_row=4
 
 fp=open("bulletin.txt","a")
 
-#mylcd=I2C_LCD_driver.lcd()
-#mylcd.lcd_display_string("Hello I2C World!", 1)
+mylcd=I2C_LCD_driver.lcd()
 
-row = [ [ "_" for y in range(disp_row) ] for x in range(disp_col) ] 
+row = [ [ "_" for y in range(disp_row+1) ] for x in range(disp_col+1) ] 
 
 def print_display(row):
     system("clear")
     for j in range(disp_row):
     	for i in range(disp_col):
         	print(row[i][j],end='')
-        	#mylcd.lcd_display_string(row[i][j], j, i)
+        	mylcd.lcd_display_string(row[i][j], j+1, i)
     	print("")
 
 ##########################################
@@ -27,26 +27,25 @@ def print_display(row):
 x=0
 while True:
     c=sys.stdin.read(1)
-    sleep(0.1)
+    sleep(0.2)
     print(c,file=fp,end='')
 
     if c.isprintable():
-        row[x][disp_row-1]=c
+        row[x][disp_row]=c
     elif ord(c)==10:
         x=disp_col
     else:
-        row[x][disp_row-1]="~"
+        row[x][disp_row]="~"
 
     # print new char on the last line of display
-    # mylcd.lcd_display_string(row[x][disp_row-1], disp_row-1, x)
+    mylcd.lcd_display_string(row[x][disp_row], disp_row, x)
 
     x=x+1
     if x>=(disp_col):
         x=0
         for i in range(disp_col):
-            for k in (range(disp_row-1)): 
+            for k in (range(disp_row)): 
                 row[i][k]=row[i][k+1]
-            row[i][disp_row-1]="_"
-
-    print_display(row)
+            row[i][disp_row]=" "
+        print_display(row)
 
